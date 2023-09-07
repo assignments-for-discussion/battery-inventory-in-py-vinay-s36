8d7ad34edf434123b2e68240a25578a0b247ed40
Sudeep Prasad <sudeep.rp@numocity.com>
2023-09-07 13:46:37 +0530

    SoH

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 
-def count_batteries_by_usage(cycles):
+def count_batteries_by_health(present_capacities):
   return {
-    "lowCount": 0,
-    "mediumCount": 0,
-    "highCount": 0
+    "healthy": 0,
+    "exchange": 0,
+    "failed": 0
   }
 
 
-def test_bucketing_by_number_of_cycles():
-  print("Counting batteries by usage cycles...\n");
-  counts = count_batteries_by_usage([100, 300, 500, 600, 900, 1000])
-  assert(counts["lowCount"] == 2)
-  assert(counts["mediumCount"] == 3)
-  assert(counts["highCount"] == 1)
+def test_bucketing_by_health():
+  print("Counting batteries by SoH...\n")
+  present_capacities = [115, 118, 80, 95, 91, 77]
+  counts = count_batteries_by_health(present_capacities)
+  assert(counts["healthy"] == 2)
+  assert(counts["exchange"] == 3)
+  assert(counts["failed"] == 1)
   print("Done counting :)")
 
 
 if __name__ == '__main__':
-  test_bucketing_by_number_of_cycles()
+  test_bucketing_by_health()
